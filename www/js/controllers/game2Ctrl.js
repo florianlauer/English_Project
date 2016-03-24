776974723e76c80145686d64231bbc7f3d172715
KacedDyum <florian.lauer@telecomnancy.net>
2016-03-24 16:35:15 +0100

    Suite jeu 2

--- www/js/controllers/game2Ctrl.js
+++ www/js/controllers/game2Ctrl.js
@@ -1,34 +1,131 @@
 
-app.controller('game2Ctrl', function ($scope, bddFactory) {
+app.controller('game2Ctrl', function ($scope, bddFactory, $ionicPopup) {
 
 		var questions =  bddFactory.getExpressions();
 		$scope.questions = questions;
+		var questionPassed = [];
+		var bottomPassed;
+		$scope.answerRem = 6;
+		$scope.remaining = 10;
+		$scope.question = selectQuestion();
 		console.log(questions[0]);
 
 
 
+		/*
+		On selectionne une phrase en langage1
+		On selectionne la phrase correspondante en langage 2
+		On selectionne 4 autres phrases !!différentes!! en langage2, on les fous dans un tableau
+		Si c'est bon : OK
+		Sinon : On prend une phrase au hasard dans les 4 restantes
+
+		*/
+
+		$scope.answer = function answer() {
+
+
+			var answer;
+
+			if($scope.remaining == 1){
+				var endMessage = {
+				 template: ' <p style="font-weight : bold; border-bottom : 1px green solid;";> <span class="icon ion-ios-star" style = "color: green; font-weight : 60px; font-size : 30px;"></span> CONGRATULATIONS <span class="icon ion-ios-star" style = "color: green; font-weight : 60px; font-size : 30px;"></span></p><p>SCORE : '+ $scope.score +'</p>',
+
+			 };
 
 
 
-
-	function selectQuestion() {
-
-
-
-		var randId = Math.floor(Math.random() * expressions.length);
-
-
-		while(questionPassed.indexOf(randId) != -1){
-
-			randId = Math.floor(Math.random() * expressions.length);
-
+			$ionicPopup.alert(endMessage);
+ 	    $state.go('rules_2');
+ 	    $scope.score = 0;
+ 	    $scope.remaining = 10;
 		}
+		else{
 
-		questionPassed.push(randId);
+	    $scope.remaining--;
 
-		return expressions[randId];
+	  }
+
+		var message;
+
+		if(answer == $scope.question.response){
+	    $scope.score++;
+
+	    message = 'yay!';
+
+	  }else{
+	    message = 'tu es nul';
+	  }
+	  $ionicPopup.alert(message);
+
+	  $scope.answer();
 
 	}
+
+
+
+
+		function selectQuestion() {
+
+
+			// ID random d'une question
+		  var randQuestionId = Math.floor(Math.random() * questions.length);
+			console.log(randQuestionId);
+
+		  while(questionPassed.indexOf(randQuestionId) != -1){ // Si cet ID est dans le tableau des questions passées, on en choisit un nouveau
+
+		    randQuestionId = Math.floor(Math.random() * questions.length);
+
+		  }
+
+		  questionPassed.push(randQuestionId); // Et on l'ajoute aux questions passées
+
+			var question = questions[randQuestionId];
+			var goodAnsId = randQuestionId;
+			var answers = [];
+			var goodAns = questions[randQuestionId].french;
+			answers.push(goodAns);
+			console.log('init TAB = '+ answers);
+			var i =1;
+			while (i<6) {
+				randId = Math.floor(Math.random() * questions.length);
+				answer = questions[randId].french; // réponse aléatoire parmis toutes les expressions
+				if (answers.indexOf(answer) === -1) {
+					answers.push(answer);
+					i++;
+					console.log('Le nouveau tableau est : ' + answers);
+				}
+				else if ( answers.indexOf(answer) > -1 ) {
+					 console.log(answer + ' existe déjà dans le tableau.');
+				}
+			}
+			console.log('FINAL '+ answers.toString());
+			console.log(answers.length);
+
+
+			var mixAnswers = ['','','','','',''];
+		  var mixedAnswers = [];
+		  i = 0;
+
+		  while(mixedAnswers.length < 6){
+
+		    do{
+
+		      randId = Math.floor(Math.random() * 6);
+
+		    }while(mixedAnswers.indexOf(randId) != -1)
+
+		    mixedAnswers.push(randId);
+		    mixAnswers[randId] = answers[i];
+		    i++;
+		  }
+
+			console.log(mixAnswers);
+			console.log(mixAnswers.length);
+
+
+		  return {'question' : question, 'response' : goodAns, 'answers' : mixAnswers};
+
+		  }
 
 
 });
